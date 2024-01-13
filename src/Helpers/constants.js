cf254cb1c54d8fab54c483070f94a05174a188c7
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2024-01-13 16:46:29 +0100

    json

---
+++ src/Helpers/constants.js
@@ -0,0 +1,9 @@
+export const apiKey = '0bb5e486969da0e081671e4e0fafc6ed';
+export const url = 'https://api.themoviedb.org/3';
+export const AUTH_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmI1ZTQ4Njk2OWRhMGUwODE2NzFlNGUwZmFmYzZlZCIsInN1YiI6IjY1OWZmZjQ1YWFmODk3MDEyYTk3NjczNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8-btsOXoCpsSkAqSg9eOIZOtl2Ch2NcBZuqc1EjWhQ8';
+export  const STATUSES = {
+    idle: "idle",
+    pending: "pending",
+    success: "success",
+    error: "error",
+}
